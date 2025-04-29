721d45d2915956bbb0c6fc6a80cb48e91fad45e5
unknown <s283814@studenti.polito.it>
2025-04-29 13:35:26 +0200

    aggiunta file sorgente

---
+++ build2/CMakeFiles/Sorting2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sorting2.dir/main.cpp.o"
+  "CMakeFiles/Sorting2.dir/main.cpp.o.d"
+  "CMakeFiles/Sorting2.dir/src/sorts.cpp.o"
+  "CMakeFiles/Sorting2.dir/src/sorts.cpp.o.d"
+  "Sorting2"
+  "Sorting2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sorting2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build2/CMakeFiles/Sorting2_TEST.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sorting2_TEST.dir/main_test.cpp.o"
+  "CMakeFiles/Sorting2_TEST.dir/main_test.cpp.o.d"
+  "CMakeFiles/Sorting2_TEST.dir/src/sorts.cpp.o"
+  "CMakeFiles/Sorting2_TEST.dir/src/sorts.cpp.o.d"
+  "Sorting2_TEST"
+  "Sorting2_TEST.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sorting2_TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
